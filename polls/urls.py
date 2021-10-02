7fc9126d61d509ddbcb9e86ee7149001d8dab8c7
maxim1106 <mrobota@livegenic.com>
2021-10-02 18:44:32 +0300

    Create listCreate+ RetreaveUpdateDelete views based on choise model

--- polls/urls.py
+++ polls/urls.py
@@ -11,6 +11,10 @@ urlpatterns = [
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
+
     path('question/', views.QuestionList.as_view(), name='QuestionList'),
-    path('question/<int:question_id>/', views.QuestionDetails.as_view(), name='QuestionDetails')
+    path('question/<int:question_id>/', views.QuestionDetails.as_view(), name='QuestionDetails'),
+
+    path('choice/', views.ChoiceList.as_view(), name='ChoiceList'),
+    path('choice/<int:choice_id>/', views.ChoiceDetails.as_view(), name='ChoiceDetails')
 ]
